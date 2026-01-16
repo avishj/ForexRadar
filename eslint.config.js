09135da55854676b44503700c10bb3240ae673c7
Avish Jha <avish.j@protonmail.com>
2026-01-16 14:00:56 -0500

    lint: setup linter for the project
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        Bun: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+      "prefer-const": "warn",
+      eqeqeq: ["warn", "smart"],
+    },
+  },
+  {
+    ignores: ["node_modules/**", "db/**", "*.min.js"],
+  },
+];
