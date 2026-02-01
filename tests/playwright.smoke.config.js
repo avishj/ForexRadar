fd48d9bfc2bfd8daad6bc46b13ca5440cd78d6a7
Avish Jha <avish.j@protonmail.com>
2026-02-01 04:00:29 -0500

    test: 4 workers smoke
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- tests/playwright.smoke.config.js
+++ tests/playwright.smoke.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 4 : 2,
   reporter: [['list'], ['html', { open: 'never' }]],
   timeout: 45000,
   
